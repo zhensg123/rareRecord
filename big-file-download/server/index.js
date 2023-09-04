8b56c82e2347ecc2041bb429f2402a1604fa270b
zhensg123 <xyqblog@163.com>
2023-09-04 21:23:58 +0800

    初始化大文件下载

---
+++ big-file-download/server/index.js
@@ -0,0 +1,183 @@
+const express = require('express');
+const app = express();
+
+const multiparty = require("multiparty");
+const path = require('path');
+const fse = require("fs-extra");
+
+// 跨域设置
+const cors = require('cors')
+app.use(cors())
+
+const UPLOAD_DIR = path.resolve(__dirname, ".", "target");
+// 提取后缀名
+// get file extension
+const extractExt = fileName => fileName.slice(fileName.lastIndexOf("."), fileName.length);
+
+// 创建临时文件夹用于临时存储 chunk
+// 添加 chunkDir 前缀与文件名做区分
+// create a directory for temporary storage of chunks
+// add the 'chunkDir' prefix to distinguish it from the chunk name
+const getChunkDir = fileHash =>
+  path.resolve(UPLOAD_DIR, `chunkDir_${fileHash}`);
+
+
+// 返回已上传的所有切片名
+// return chunk names which is uploaded
+const createUploadedList = async fileHash =>
+  fse.existsSync(getChunkDir(fileHash))
+    ? await fse.readdir(getChunkDir(fileHash))
+    : [];
+
+const resolvePost = req =>
+  new Promise(resolve => {
+    let chunk = "";
+    req.on("data", data => {
+      chunk += data;
+    });
+    req.on("end", () => {
+      resolve(JSON.parse(chunk));
+    });
+  });
+
+// 写入文件流
+const pipeStream = (path, writeStream) =>
+  new Promise(resolve => {
+    const readStream = fse.createReadStream(path);
+    readStream.on("end", () => {
+      fse.unlinkSync(path);
+      resolve();
+    });
+    readStream.pipe(writeStream);
+  });
+
+// 合并切片
+const mergeFileChunk = async (filePath, fileHash, size) => {
+  const chunkDir = getChunkDir(fileHash);
+  // 读取所有chunk路径
+  const chunkPaths = await fse.readdir(chunkDir);
+  // 根据切片下标进行排序
+  // 使用下面的方式有问题
+  // chunkPaths.sort((a, b) => parseInt(a.split('-')[1]) - parseInt(b.split('-')[1]));
+  chunkPaths.sort((a, b) => parseInt(a.split('-').pop()) - parseInt(b.split('-').pop()));
+  // 并发写入文件
+  await Promise.all(
+    chunkPaths.map((chunkPath, index) =>
+      pipeStream(
+        path.resolve(chunkDir, chunkPath),
+        // 根据 size 在指定位置创建可写流
+        fse.createWriteStream(filePath, {
+          start: index * size,
+        })
+      )
+    )
+  );
+  // 合并后删除保存切片的目录
+  fse.rmdirSync(chunkDir);
+};
+
+
+app.post('/file/upload', function (req, res, next) {
+
+  const multipart = new multiparty.Form();
+  multipart.parse(req, async function (err, fields, files) {
+      if(err){
+        res.status = 500
+        res.end("异常错误");
+        return
+      }
+      const [chunk] = files.chunk;
+      const [hash] = fields.hash; // 切片hash
+      const [fileHash] = fields.fileHash;
+      const [fileName] = fields.fileName;
+      // 获取文件路径
+      const filePath = path.resolve(
+        UPLOAD_DIR,
+        `${fileHash}${extractExt(fileName)}`
+      );
+      // path.resolve 将相对路径解析为绝对路径 切片名改为fileHash
+      const chunkDir = getChunkDir(fileHash);
+      const chunkPath = path.resolve(chunkDir, hash);
+
+      // 文件存在直接返回
+      // return if file is exists
+      if (fse.existsSync(filePath)) {
+        res.end("file exist");
+        return;
+      }
+
+      // 切片存在直接返回
+      // return if chunk is exists
+      if (fse.existsSync(chunkPath)) {
+        res.end("chunk exist");
+        return;
+      }
+
+      // 切片目录不存在，创建切片目录
+      // if chunk directory is not exist, create it
+      if (!fse.existsSync(chunkDir)) {
+        await fse.mkdirs(chunkDir);
+      }
+
+    try {
+      // fs-extra 的 move 方法用于移动文件或目录。
+      await fse.move(chunk.path, path.resolve(chunkDir, hash));
+    } catch (err) {
+      console.log(err)
+    }
+    res.end("received file chunk");
+  });
+});
+
+
+
+
+app.post('/file/verify', async function (req, res, next) {
+  const data = await resolvePost(req);
+  const { fileHash, fileName } = data;
+  const ext = extractExt(fileName);
+  const filePath = path.resolve(UPLOAD_DIR, `${fileHash}${ext}`);
+  if (fse.existsSync(filePath)) {
+    res.end(
+      JSON.stringify({
+        shouldUpload: false
+      })
+    );
+  } else {
+    res.end(
+      JSON.stringify({
+        shouldUpload: true,
+        uploadedList: await createUploadedList(fileHash)
+      })
+    );
+  }
+});
+app.post('/file/merge', async function (req, res, next) {
+  const data = await resolvePost(req);
+  const { fileHash, fileName, size } = data;
+  const ext = extractExt(fileName);
+  const filePath = path.resolve(UPLOAD_DIR, `${fileHash}${ext}`);
+  await mergeFileChunk(filePath, fileHash, size);
+  res.end(
+    JSON.stringify({
+      code: 0,
+      message: "file merged success"
+    })
+  );
+});
+app.get('/file/delete', async function (req, res, next) {
+   await fse.remove(path.resolve(UPLOAD_DIR));
+    res.end(
+      JSON.stringify({
+        code: 0,
+        message: "file delete success"
+      })
+    );
+});
+
+app.get('/test', async function (req, res, next) {
+   res.send('测试2')
+});
+app.listen(3000, function () {
+  console.log('hello world')
+})
---
+++ big-file-download/src/views/ResumeUpload.vue
@@ -0,0 +1,341 @@
+<template>
+  <div>
+    <input
+      type="file"
+      ref="fileUploadInput"
+      @change="uploadFileChange"
+    />
+    <el-button @click="uploadChunks" :disabled="uploadDisabled"
+      >分片上传</el-button
+    >
+    <el-button @click="resumeUpload" v-if="status === Status.pause"
+      >恢复上传</el-button
+    >
+    <el-button
+        v-else
+        :disabled="status !== Status.uploading || !fileData.fileHash"
+        @click="pauseUpload"
+        >暂停上传</el-button
+      >
+    <el-button @click="handleDelete">删除所有上传文件</el-button>
+    <el-form label-width="100px" label-position="top">
+      <el-form-item label="计算切片hash：">
+        <el-progress :percentage="hashPercentage"></el-progress>
+      </el-form-item>
+    </el-form>
+    <el-form label-width="100px" label-position="top">
+      <el-form-item label="上传总进度：">
+        <el-progress :percentage="fakeUploadPercentage"></el-progress>
+      </el-form-item>
+    </el-form>
+    <el-table :data="uploadChunkData" style="width: 100%">
+      <el-table-column prop="hash" label="chunk hash" width="400">
+      </el-table-column>
+      <el-table-column prop="size" label="size(Mb)" width="180">
+        <template slot-scope="{ row }">
+          {{ row.size | transformByte }}
+        </template>
+      </el-table-column>
+      <el-table-column min-width="180" prop="percentage" label="percentage">
+        <template slot-scope="{ row }">
+          <el-progress :percentage="row.percentage"></el-progress>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const CancelToken = axios.CancelToken;
+
+const SIZE = 10 * 1024 * 1024;
+const LIMIT = 6;
+const Status = {
+  wait: "wait",
+  pause: "pause",
+  uploading: "uploading",
+};
+export default {
+  name: "BigFIleUpload",
+  components: {},
+  data() {
+    this.chunkRequestList = []
+    this.uploadedList = []
+    return {
+      Status,
+      uploadChunkData: [],
+      fileData: {
+        file: null,
+        fileHash: "",
+        worker: null,
+      },
+      status: Status.wait,
+      fakeUploadPercentage: 0,
+      hashPercentage: 0
+    };
+  },
+  filters: {
+    transformByte(val) {
+      return Number((val / 1024 / 1024).toFixed(0));
+    },
+  },
+  computed: {
+    uploadPercentage() {
+      if (!this.fileData.file || !this.uploadChunkData.length) return 0;
+      const loaded = this.uploadChunkData
+        .map((item) => item.size * item.percentage)
+        .reduce((acc, cur) => acc + cur);
+      return parseInt((loaded / this.fileData.file.size).toFixed(2));
+    },
+    uploadDisabled() {
+      return (
+        !this.fileData.file ||
+        [Status.pause, Status.uploading].includes(this.status)
+      );
+    },
+  },
+  watch: {
+    uploadPercentage(now) {
+      if (now > this.fakeUploadPercentage) {
+        this.fakeUploadPercentage = now;
+      }
+    },
+  },
+  methods: {
+    // 上传后判断类型
+    uploadFileChange(event) {
+      const [file] = event.target.files;
+      this.fileData.file = file;
+      const { ext } = this.getFileNameAndExt();
+      if (!this.checkFileType(ext)) {
+        this.$refs.fileUploadInput.value = "";
+        return this.$message({
+          type: "warning",
+          message: "文件上传错误",
+        });
+      }
+    },
+    pauseUpload() {
+      this.status = Status.pause;
+      this.resetUploadData();
+    },
+    // 生成文件 hash（web-worker）
+    calculateHash(fileChunkList) {
+      return new Promise((resolve) => {
+        this.fileData.worker = new Worker("/js/hash.js");
+        this.fileData.worker.postMessage({ fileChunkList });
+        this.fileData.worker.onmessage = (e) => {
+          const { percentage, hash } = e.data;
+          this.hashPercentage = percentage;
+          if (hash) {
+            resolve(hash);
+          }
+        };
+      });
+    },
+    async handleDelete() {
+      await axios({
+        method: "get",
+        url: "http://localhost:3000/file/delete",
+      });
+      this.$refs.fileUploadInput.value = this.fileData.file =  "";
+      this.status = Status.wait;
+      this.$message.success("删除成功");
+    },
+    resetUploadData() {
+      this.chunkRequestList.forEach((cancel) => cancel());
+      this.chunkRequestList = [];
+      if (this.fileData.worker) {
+        this.fileData.worker.onmessage = null;
+      }
+    },
+    async resumeUpload() {
+      if (!this.fileData.file || !this.uploadChunkData.length) return 0;
+
+      const { uploadedList, shouldUpload } = await this.verifyUpload(
+        this.fileData.file.name,
+        this.fileData.fileHash
+      );
+      if(!shouldUpload){
+        this.$message.success(
+          "skip upload：file upload success, check /target directory"
+        );
+        this.status = Status.wait;
+        return;
+      }
+      this.uploadedList = uploadedList
+      this.startShouldFileUpload();
+    },
+
+    async verifyUpload(fileName, fileHash) {
+      const { data } = await axios({
+        method: "post",
+        url: "http://localhost:3000/file/verify",
+        headers: {
+          "content-type": "application/json",
+        },
+        data: JSON.stringify({
+          fileName,
+          fileHash,
+        }),
+      });
+      return data
+    },
+    // 获取文件名和扩展名
+    getFileNameAndExt() {
+      const { name } = this.fileData.file;
+      return {
+        fileName: name,
+        ext: name.slice(name.lastIndexOf(".") + 1),
+      };
+    },
+    checkFileType(ext) {
+      return ["mp4", "docx", "doc", "xlsx"].includes(ext);
+    },
+    // file大文件，size切片的大小
+    createChunk(file, size = SIZE) {
+      const chunkList = [];
+      let cur = 0;
+      while (cur < file.size) {
+        chunkList.push({
+          file: file.slice(cur, cur + size), //使用slice()进行切片
+        });
+        cur += size;
+      }
+      return chunkList;
+    },
+    // 上传文件：创建切片、验证是否需要上传
+    async uploadChunks() {
+      this.status = Status.uploading;
+
+      // 创建切片
+      const chunkList = this.createChunk(this.fileData.file);
+      this.fileData.fileHash = await this.calculateHash(chunkList);
+
+      const { shouldUpload, uploadedList } = await this.verifyUpload(
+        this.fileData.file.name,
+        this.fileData.fileHash
+      );
+
+      if (!shouldUpload) {
+        this.$message.success(
+          "skip upload：file upload success, check /target directory"
+        );
+        this.status = Status.wait;
+        return;
+      }
+      this.uploadedList = uploadedList
+      // 创建上传切片数据
+      this.uploadChunkData = chunkList
+        .filter(({ hash }) => !this.uploadedList.includes(hash))
+        .map(({ file }, index) => ({
+          fileHash: this.fileData.fileHash,
+          chunk: file, // 切片文件
+          hash:  `${this.fileData.fileHash}-${index}`,
+          percentage: this.uploadedList.includes(index) ? 100 : 0,
+          size: file.size,
+          index,
+        }));
+
+      this.startShouldFileUpload();
+    },
+    // 发起请求：创建切片请求、发起请求、并发控制、发起合并切片
+   startShouldFileUpload() {
+      // 创建切片请求
+      const chunksRequests = this.createChunksRequest();
+
+      // 切片为0直接合并
+      if(chunksRequests.length === 0){
+        return this.mergeRequest()
+      }
+      // 并发以及并发控制
+      this.requestWithLimit(chunksRequests, () => this.mergeRequest());
+    },
+    mergeRequest() {
+      axios({
+        method: "post",
+        url: "http://localhost:3000/file/merge",
+        headers: {
+          "content-type": "application/json",
+        },
+        data: JSON.stringify({
+          size: SIZE,
+          fileHash: this.fileData.fileHash,
+          fileName: this.getFileNameAndExt().fileName,
+        }),
+      });
+    },
+    requestWithLimit(prmiseQueue, callback = null) {
+      // 请求数量记录，默认为 0
+      let count = 0;
+      // 递归调用，请求接口数据
+      const run = () => {
+        // 接口每调用一次，记录数加 1
+        count++;
+        const p = prmiseQueue.shift();
+        p.then((res) => {
+          console.log(res, 'pres')
+
+          // 接口调用完成，记录数减 1
+          count--;
+          if (!prmiseQueue.length && !count) {
+            // 这里可以对所有接口返回的数据做处理，以便输出
+            callback && callback();
+          }
+          // prmiseQueue 长度不为 0 且记录小于限制的数量时递归调用
+          if (prmiseQueue.length && count < LIMIT) {
+            run();
+          }
+        });
+      };
+
+      // 根据 limit 并发调用
+      for (let i = 0; i < Math.min(prmiseQueue.length, LIMIT); i++) {
+        run();
+      }
+    },
+    createChunksRequest() {
+      return this.uploadChunkData
+        .filter(({ hash }) => !this.uploadedList.includes(hash))
+        .map(({ chunk, hash, index }) => {
+          const formData = new FormData();
+          formData.append("chunk", chunk);
+          formData.append("hash", hash);
+          formData.append("fileName", this.getFileNameAndExt().fileName);
+          formData.append("fileHash", this.fileData.fileHash);
+
+          return { formData, index };
+        })
+        .map(({ formData, index }) => {
+          return new Promise((resolve, reject) => {
+            axios({
+              method: "post",
+              url: "http://localhost:3000/file/upload",
+              data: formData,
+              onUploadProgress: (progressEvent) => {
+                let complete = parseInt(
+                  (progressEvent.loaded / progressEvent.total) * 100
+                );
+                this.uploadChunkData[index].percentage = complete;
+              },
+              // 用于取消请求
+              cancelToken: new CancelToken((cancel)=> {
+                this.chunkRequestList.push(cancel);
+              }),
+            })
+              .then((res) => {
+                if (this.chunkRequestList) {
+                  this.chunkRequestList.splice(index, 1);
+                }
+                resolve(res);
+              })
+              .catch((err) => {
+                reject(err);
+              });
+          });
+        });
+    },
+  },
+};
+</script>
