8b2ab998bc7ea2df13a6cde484cedb94a35c5d20
zhenshigang <zhenshigang@xdf.cn>
2023-09-08 17:34:02 +0800

    开发初级版本的vue

---
+++ simple-simple-vue/index.js
@@ -0,0 +1,14 @@
+class  SSvue {
+    constructor(options){
+       const {btnText,alertSomeThingText, el } = options
+
+    }
+
+    getTemplateHtml(el){
+        const template = document.getElementById(el)
+    }
+
+    compileTemplate(template){
+
+    }
+}
